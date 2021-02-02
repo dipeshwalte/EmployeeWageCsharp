d1b1ee995ff44dc74de80e91f662289ce57d5bd7
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-02 12:32:36 +0530

    Add project files.

---
+++ EmployeeWageCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWageCsharp
+{
+	public class Program
+	{
+		public static void Main(String[] args)
+		{
+            Console.WriteLine("Welcome to employee wage computation!");
+		}
+	}
+}
