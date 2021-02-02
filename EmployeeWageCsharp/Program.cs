67c37a62e5ba6cb20bef1cdc69033162ad4519ae
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-02 12:59:24 +0530

    [Dipesh] modify AddedDailyWageLocic

--- EmployeeWageCsharp/Program.cs
+++ EmployeeWageCsharp/Program.cs
@@ -5,16 +5,22 @@ namespace EmployeeWageCsharp
 	public class Program
 	{
 		const int PRESENT = 1;
-		const int ABSENT = 2;
+		const int WAGE_PER_HOUR = 20;
+		const int FULL_DAY_HOUR = 8;
 		public static void Main(String[] args)
 		{
 			Console.WriteLine("Welcome to employee wage computation!");
+			
 			Random rand = new Random();
 			int attendance = rand.Next(0, 2);
-            if (attendance==PRESENT)
-                Console.WriteLine("Employee Present");
-			else
-                Console.WriteLine("Employee Absent");
+			int dailywage = 0;
+
+			if (attendance == PRESENT)
+            {
+				dailywage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+			
+			}
+			Console.WriteLine($"Daily Wage : {dailywage}");
 		}
 	}
 }
